0b7c9d0c78d2aefab705c5fe1e344311075cb051
mani <227445662+0xmanidev@users.noreply.github.com>
2026-07-19 01:12:54 +0530

    just history

---
+++ src/helper/history.js
@@ -0,0 +1,14 @@
+export function addHistory(number){
+    const list = document.getElementById("historyList");
+
+    const div = document.createElement("div");
+    
+    div.className = "historyItem";
+
+    div.textContent = number;
+
+    list.prepend(div);
+}
+export function clearHistory(){
+    document.getElementById("historyList").innerHTML="";
+}
